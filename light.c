f253afbccf8dca1d5fdf54e3b012f974acdc09ae
viforget <42910751+viforget@users.noreply.github.com>
2020-11-13 10:32:48 +0100

    Advance in light

--- light.c
+++ light.c
@@ -12,6 +12,13 @@
 
 #include "MiniRT.h"
 
+int		verif(float dist_obj, float dist_light)
+{
+	if (dist_obj == -1 || dist_obj - ZE > dist_light)
+		return (0);
+	return (1);
+}
+
 int		calc_light(float c[3], int color, t_arg arg)
 {
 	t_lig	*light;
@@ -20,7 +27,7 @@ int		calc_light(float c[3], int color, t_arg arg)
 	float	v[3];
 	int 	ret;
 	float	dist;
-
+	
 	ret = light_color(color, arg.a_rat, arg.a_color);
 	light = arg.lig;
 	while(light)
@@ -28,15 +35,21 @@ int		calc_light(float c[3], int color, t_arg arg)
 		vect_to(c, light->c, v, &dist);
 		obj = arg.obj;
 		check = 1;
-		while(obj)
+		while(obj && check == 1)
 		{
-			if (obj->type == SP)
-			printf("%f %f\n", dist_obj(obj, v, c), dist);
-			if (dist_obj(obj, v, c) > dist)
-				check = 0;	
+			/*if (obj->type == SP)
+			{
+				printf("%f %f\n", dist_obj(obj, v, c), dist);
+				printf("%f %f %f\n", v[X], v[Y], v[Z]);
+			}*/
+			if (verif(dist_obj(obj, v, c), dist))
+			{
+				printf("%f %f\n", dist_obj(obj, v, c), dist);
+				check = 0;
+			}
 			obj = obj->next;
 		}
-		if (check)
+		if (check == 1)
 			ret = light_color(color, light->rat, light->color);
 		light = light->next;
 	}
